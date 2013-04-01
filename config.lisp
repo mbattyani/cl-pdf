f9d6c83cc065894a876df99d94c95928e35e461f
Dave Cooper <open-source@genworks.com>
2013-04-01 16:35:13 -0400

    added clisp to #- list for defconstant +external-format+

--- config.lisp
+++ config.lisp
@@ -8,7 +8,7 @@
 ;;; depending on your Lisp implementation/OS/installation.
 
 (defconstant +external-format+
-  #-(or sbcl lispworks clisp allegro) :default
+  #-(or sbcl lispworks clisp allegro ccl) :default
   #+abcl '(:iso-8859-1 :eol-style :lf)
   #+ecl '(:latin-1 :lf)
   #+ccl :latin1
